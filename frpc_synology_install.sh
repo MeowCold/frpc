381c711b90b26a35f9fd8cc520f1bffd9f1f4b4f
MeowCold <MeowCold@Gmail.com>
2020-03-15 19:48:40 +0800

    arm64
    arm64

--- frpc_synology_install.sh
+++ frpc_synology_install.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 FRP_VERSION="0.31.2"
-REPO="stilleshan/frpc"
+REPO="MeowCold/frpc"
 WORK_PATH=$(dirname $(readlink -f $0))
 
 # 创建frp文件夹
 mkdir -p /usr/local/frp && \
 # 下载并移动frpc文件
-wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz && \
-tar -zxvf frp_${FRP_VERSION}_linux_amd64.tar.gz && \
-cd frp_${FRP_VERSION}_linux_amd64 && \
+wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_arm64.tar.gz && \
+tar -zxvf frp_${FRP_VERSION}_linux_arm64.tar.gz && \
+cd frp_${FRP_VERSION}_linux_arm64 && \
 mv frpc /usr/local/frp && \
 # 下载frpc.in
 wget -P /usr/local/frp https://raw.githubusercontent.com/${REPO}/master/frpc.ini && \
 # 删除多余文件
 cd ${WORK_PATH} && \
-rm -rf frp_${FRP_VERSION}_linux_amd64 frp_${FRP_VERSION}_linux_amd64.tar.gz && \
+rm -rf frp_${FRP_VERSION}_linux_arm64 frp_${FRP_VERSION}_linux_arm64.tar.gz && \
 rm -rf ${WORK_PATH}/frpc_synology_install.sh
 # 完成安装,手动修改frpc.ini并启动服务.
 echo "======================================================================" &&\
